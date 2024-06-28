50cc3528cc813f705d6c4d578fe928b5993faffb
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-06-28 15:20:43 +0200

    Updated Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,25 +10,24 @@ interface Movies {
 }
 
 export default function Card({ movies }: Movies) {
-    
     return (
-        <section className="grid-cols-1 gap-4 md:grid-cols-4">
-            {
-                movies.map((card) => (
-                    <div 
-                    key={ card.imdbID } 
-                    className="p-3 border rounded-lg">
-                        <div className="">
-                            <img 
-                            src={ card.Poster } 
-                            className="rounded-lg" 
-                            alt={ card.Title } />
-                    <h5 className="">{ card.Title }</h5>
-                    <p className="">{ card.Year }</p>
+        <section className="grid grid-cols-1 gap-4 md:grid-cols-4">
+        {movies.map((card) => (
+            <div 
+            key={ card.imdbID } // La clé qui rend unique l'élément 
+            className="p-3 transition-shadow duration-200 ease-in-out border rounded-lg shadow-md bg-slate-50 hover:shadow-lg">
+                <div className="">
+                    <img 
+                        src={ card.Poster } 
+                        className="rounded-lg" 
+                        alt={ card.Title } />
+                        <div className="flex flex-col justify-between p-2">
+                            <h5 className="text-xl font-light leading-relaxed">{ card.Title }</h5>
+                            <p className="text-center">{ card.Year }</p>
                         </div>
-                    </div>
-                ))
-            }
-        </section>        
-    );
+                </div>
+            </div>
+        )) }
+        </section>
+    )
 }
